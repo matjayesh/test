fa1331cb938a246d259d1b0515b729e057006819
Jayesh <matjayesh@gmail.com>
2017-09-19 12:06:49 +0530

    test

---
+++ routes/books.js
@@ -0,0 +1,146 @@
+'use strict';
+
+const Boom = require('boom');  
+const uuid = require('node-uuid');  
+const Joi = require('joi');
+
+exports.register = function(server, options, next) {
+
+  const db = server.app.db;
+	
+  //PLACEHOLDER
+  //--------------------------------------------------------------
+  server.route({
+        method: 'GET',
+        path: '/books',
+        handler: function (request, reply) {
+
+            db.cards.find((err, docs) => {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                reply(docs);
+            });
+
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/books/{Token}',
+        handler: function (request, reply) {
+			console.log("request.params.Token::"+request.params.Token)
+            db.cards.findOne({
+                Token: request.params.Token
+            }, (err, doc) => {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                if (!doc) {
+                    return reply(Boom.notFound());
+                }
+
+                reply(doc);
+            });
+
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/books',
+        handler: function (request, reply) {
+	console.log("request::"+JSON.stringify(request.params));
+            const book = request.payload;
+			console.log("request::"+JSON.stringify(request));
+			
+			
+            //Create an id
+            book._id = uuid.v1();
+
+            db.books.save(book, (err, result) => {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                reply(book);
+            });
+        },
+        config: {
+            validate: {
+                payload: {
+                    title: Joi.string().min(10).max(50).required(),
+                    author: Joi.string().min(10).max(50).required(),
+                    isbn: Joi.number()
+                }
+            }
+        }
+    });
+
+    server.route({
+        method: 'PATCH',
+        path: '/books/{id}',
+        handler: function (request, reply) {
+
+            db.books.update({
+                _id: request.params.id
+            }, {
+                $set: request.payload
+            }, function (err, result) {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                if (result.n === 0) {
+                    return reply(Boom.notFound());
+                }
+
+                reply().code(204);
+            });
+        },
+        config: {
+            validate: {
+                payload: Joi.object({
+                    title: Joi.string().min(10).max(50).optional(),
+                    author: Joi.string().min(10).max(50).optional(),
+                    isbn: Joi.number().optional()
+                }).required().min(1)
+            }
+        }
+    });
+
+   server.route({
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: function (request, reply) {
+
+            db.books.remove({
+                _id: request.params.id
+            }, function (err, result) {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                if (result.n === 0) {
+                    return reply(Boom.notFound());
+                }
+
+                reply().code(204);
+            });
+        }
+    });
+  //Here the routes definitions will be inserted in the next steps...
+
+  return next();
+};
+
+exports.register.attributes = {  
+  name: 'routes-books'
+};
